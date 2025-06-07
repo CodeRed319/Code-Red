4faae0d2d73fd68d8a41a6deef9c4ea7437f30b1
CodeRed319 <bolulokosu273@gmail.com>
2025-06-07 06:55:54 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'K4l3b Bot',
-  author: 'caleb‎',
-  botName: "K4l3b Bot",
-  botOwner: 'Caleb', // Your name
-  ownerNumber: '2348104040841', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Code Bot',
+  author: 'bolu‎',
+  botName: "Code Bot",
+  botOwner: 'Code Red', // Your name
+  ownerNumber: '2348115676032', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
